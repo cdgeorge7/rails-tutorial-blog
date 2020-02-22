751c75d4c5e0d6c32a668e9ba9a56f188a7bd5f1
Chris George <cdgeorge7@gmail.com>
2020-02-21 21:32:53 -0600

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: 'chris', password: '1234', only: %i[destroy]
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comments_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+  private def comments_params
+    params.require(:comment).permit(:username, :body)
+  end
+end
